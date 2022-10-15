aafdf9af2a8f34228aee1479849fa794b43cc387
kamel-github <103172701+kamel-github@users.noreply.github.com>
2022-10-15 09:31:46 +0100

    dd

---
+++ tutorial_automated_testing/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Tutorial creating and using automated tests",
+    "summary": "Provides an example module for testing.",
+    "description": "Provides an example module for testing.",
+    "author": "Oocademy",
+    "website": "http://www.oocademy.com",
+    "category": "Testing",
+    "version": "15.0.0.1",
+    "depends": [
+        "base",
+        "product",
+        "sale",
+        "sale_management",
+    ],
+    'images': [
+        'static/description/banner.jpg',
+    ],
+    "demo": [
+        "data/demo_ingredients.xml",
+    ],
+    "data": [
+        "views.xml",
+        "security/ir.model.access.csv",
+    ],
+}
